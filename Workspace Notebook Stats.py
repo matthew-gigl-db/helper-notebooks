e16bff7f5c4163ff04b71ed166929ae77c498771
matthew-gigl-db <matthew-gigl-db>
2024-05-14 16:25:41 +0000

    modified created_at and modified_at timestamps and added some markdown explanation

--- Workspace Notebook Stats.py
+++ Workspace Notebook Stats.py
@@ -44,18 +44,28 @@ dbutils.widgets.text("path", "/", "Workspace Path")
 
 # MAGIC %md
 # MAGIC *** 
+# MAGIC Import the Workspace Client and the SQL service.  We won't be using the SQL service today, but it can be used to build queries for the resulting API calls.  
+# MAGIC
+# MAGIC Note that on a Databricks cluster, the WorkspaceClient class with automatically authenticate itself with the current workspace.  The access other workpsaces or to run the client outside of Databricks you'll need to authenticare with that workspace.  Please see the Python SDK documentation for more details:  
+# MAGIC
+# MAGIC [https://databricks-sdk-py.readthedocs.io/en/latest/index.html](https://databricks-sdk-py.readthedocs.io/en/latest/index.html)
 
 # COMMAND ----------
 
 # DBTITLE 1,Initialzie the Workspace Client
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service import sql
-wc = WorkspaceClient()
+w = WorkspaceClient()
 
 # COMMAND ----------
 
 # DBTITLE 1,List Notebooks in Workspace
-notebooks = wc.workspace.list(path = dbutils.widgets.get("path"), recursive=True)
+notebooks = w.workspace.list(path = dbutils.widgets.get("path"), recursive=True)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Note that *notebooks* is a generator type. Its lazy execution means that we haven't executed any API calls yet to the Databricks API.  
 
 # COMMAND ----------
 
@@ -64,12 +74,24 @@ type(notebooks)
 
 # COMMAND ----------
 
-import json
-import pandas as pd
-from pyspark.sql.functions import lit
+# MAGIC %md
+# MAGIC Import required python libriries and functions.  
 
 # COMMAND ----------
 
+# DBTITLE 1,Import methods for Spark DF Conversion
+import json
+import pandas as pd
+from pyspark.sql.functions import col, from_unixtime
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Turn the notebooks generator into a list of dictionaries, then convert to a pandas dataframe, and finally into a Spark dataframe.  
+
+# COMMAND ----------
+
+# DBTITLE 1,convert notebooks generator to pySpark dataframe
 #convert returned notebooks generator to dicts
 notebooks_as_dicts = [notebooks.as_dict() for notebooks in notebooks]
 
@@ -79,4 +101,31 @@ notebooks_df = spark.createDataFrame(notebooks_pandas)
 
 # COMMAND ----------
 
+# DBTITLE 1,Display Notebooks Dataframe
 display(notebooks_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Next we'll format the created_at and modified_at Unix times as datetime stamps using the from_unixtime Spark SQL function.  
+
+# COMMAND ----------
+
+
+notebooks_df = (
+  notebooks_df
+  .withColumn("created_at", from_unixtime(col("created_at")/1e3, 'yyyy-MM-dd HH:mm:ss'))
+  .withColumn("modified_at", from_unixtime(col("modified_at")/1e3, 'yyyy-MM-dd HH:mm:ss'))
+)
+display(notebooks_df.head(10))
+
+# COMMAND ----------
+
+# DBTITLE 1,Create TempView To Use SQL
+notebooks_df.createOrReplaceTempView("tv_notebook_details")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ***
+# MAGIC Now that we have our dataframe registered as Spark temporary view, we can switch to SQL and perform some basic analysis on our notebooks.  
